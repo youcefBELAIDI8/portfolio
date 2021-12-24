a318a902841583cf0739cc10c6a12445cb8001f5
youcefBELAIDI8 <belaidi-youcef@hotmail.com>
2021-12-24 15:19:35 +0100

    fix height project

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Img = styled.img`
   width:100%;
-  height:100%;
+  height:200px;
   object-fit: cover;
   overflow: hidden;
 `
@@ -27,6 +27,7 @@ export const BlogCard = styled.div`
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
   width: 400px;
+  height:800px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
