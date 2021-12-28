c59196c76ca8eeb5092814d3da1a632c574632da
youcefBELAIDI8 <belaidi-youcef@hotmail.com>
2021-12-28 11:01:24 +0100

    ortho

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -48,7 +48,7 @@ const Timeline = () => {
       <SectionTitle>Qui suis-je ?</SectionTitle>
       <SectionText>
       Ingénieur en télécoms et réseaux de formation et Software engineer au sein de l'entreprise Capgemini, leader mondial dans le secteur de L'IT, j'ai réalisé plusieurs applications « Client/Serveur » en utilisant les technologies modernes telles que Node Js, React et Angular. 
-      J'ai également mis en production plusieurs applications sur machines virtuelles, Cloud AWS ou encore des hébergeurs comme Netlify et Heroku. Curieux et Volontaire, Je suis prêt à relever les défis qui me seront confiés.
+      J'ai également mis en production plusieurs applications sur machines virtuelles, Cloud AWS ou encore des hébergeurs comme Netlify et Heroku. Curieux et volontaire, je suis prêt à relever les défis qui me seront confiés.
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
